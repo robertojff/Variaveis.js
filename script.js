52365044cab28a0b391a36368c91c8a1b2ae3222
robertojff <robertojff3333@gmail.com>
2023-12-15 17:51:01 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let nome = 'Roberto';
+console.log(nome);
+
+let idade = 22;
+console.log(idade);
+
+let altura = 1.70;
+console.log(altura);
+
+let sobrenome = undefined;
+console.log(sobrenome);
+
+
